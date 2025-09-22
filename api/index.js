4134684ccdb45af592a2dce0d4df70b71978162c
gadbluebird <gadbluebird@gmail.com>
2025-09-22 11:59:37 +0300

    First Project 5

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// api/index.js
-import serverless from "serverless-http";
-import app from "../Src/Server.js";
-import connectDB from "../Src/config/db.js";
-
-// نوصّل القاعدة مرة واحدة قبل تشغيل الهاندلر
-await connectDB();
-
-export const handler = serverless(app);
